bb6637278efbb42f260430b17d61d74c1f5cb4c4
Jasim Uddin <jasimbdpro@gmail.com>
2024-06-27 14:13:06 +0600

    nw

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import MainComponent from './components/MainComponent/MainComponent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,8 @@ function App() {
   return (
     <>
       <h3>Hlloe</h3>
+      <MainComponent></MainComponent>
+
     </>
   )
 }
